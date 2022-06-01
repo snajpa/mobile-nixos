0673777a5d0a5b66cb72e04ee6a1f28cc0272625
Pavel Snajdr <snajpa@snajpa.net>
2022-06-02 00:41:53 +0200

    wip

--- devices/oneplus-oneplus6/firmware/default.nix
+++ devices/oneplus-oneplus6/firmware/default.nix
@@ -11,6 +11,18 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-7CaXWOpao+vuFA7xknzbLml2hxTlmuzFCEM99aLD2uk=";
   };
   outputs = [ "out" "forPdMapper" ];
+
+
+  # Copy regulatory db and graphics firmware into this derivation, so we don't have to include those packages in the initrd
+  buildInputs = [ linux-firmware wireless-regdb ];
+  postPatch = ''
+    mkdir -p lib/firmware/qcom
+    cp ${linux-firmware}/lib/firmware/qcom/a630_gmu.bin lib/firmware/qcom/
+    cp ${linux-firmware}/lib/firmware/qcom/a630_sqe.fw  lib/firmware/qcom/
+    cp ${wireless-regdb}/lib/firmware/regulatory.db     lib/firmware/
+    cp ${wireless-regdb}/lib/firmware/regulatory.db.p7s lib/firmware/
+  '';
+
   installPhase = ''
     set -x
     mkdir -p $out/lib/firmware
